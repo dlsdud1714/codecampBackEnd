a850f0903efd0ccdb7dc5ef2ecae9c61780c6595
Inyoung Park <rkduddl1rl@gmail.com>
2022-03-13 00:04:32 -0700

    second

--- myApp.js
+++ myApp.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express= require('express');
+const app = express();
+
+const port= 5052;
+app.listen(port);
+
+app.get('/',(req,res)=>{
+    console.log("Hello World");
+    res.send("he")
+})
+
